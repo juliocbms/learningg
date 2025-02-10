2792e16ee00dd0d04b80d194d9c5a7dcdc93c403
julio <juliocesar13b@gmail.com>
2025-02-10 20:47:20 -0300

    implementando JPA e banco de teste

--- src/main/java/com/jbraga/learningg/model/entities/User.java
+++ src/main/java/com/jbraga/learningg/model/entities/User.java
@@ -1,11 +1,17 @@
 package com.jbraga.learningg.model.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
 
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private  String name;
     private String email;
