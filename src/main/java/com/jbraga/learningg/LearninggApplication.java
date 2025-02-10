baaf6c17fb4387960ffaa8b8be7ae6c46e2be018
julio <juliocesar13b@gmail.com>
2025-02-10 20:01:51 -0300

    Initial commit

---
+++ src/main/java/com/jbraga/learningg/LearninggApplication.java
@@ -0,0 +1,13 @@
+package com.jbraga.learningg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearninggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbraga/learningg/LearninggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbraga.learningg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
