b391d05cc4d985bef89a71510a1b50890aeeed19
julio <juliocesar13b@gmail.com>
2025-02-10 21:28:02 -0300

    implementando camada de serviço

---
+++ src/main/java/com/jbraga/learningg/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jbraga.learningg.services;
+
+import com.jbraga.learningg.model.entities.User;
+import com.jbraga.learningg.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+      return   repository.findAll();
+    }
+
+    public User findById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+}
